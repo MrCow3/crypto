672a81408ea25ef1a3c23c0019f5efaa9d892b1c
Alex Lorei <DrCow@protonmail.com>
2022-06-18 17:58:12 -0400

    fixed return

--- hex.go
+++ hex.go
@@ -5,7 +5,7 @@ import (
 	"fmt"
 )
 
-func DecodeHex(hexString string) {
+func DecodeHex(hexString string) string {
 	decoded, err := hex.DecodeString(hexString)
 	if err != nil {
 		fmt.Println("Error:", err, "make sure you import a hex string")
